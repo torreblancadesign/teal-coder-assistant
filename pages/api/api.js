dc09f33f4260c13b1b3ed04ba24224856db0954b
Enrique Torreblanca <enriquetchat@gmail.com>
2023-06-18 00:05:15 -0700

    Added a scrapping serverless function

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,2 +1,29 @@
-//Not needed.
+// This is a Node.js script, which can be deployed as a Vercel Serverless Function
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+module.exports = async (req, res) => {
+  try {
+    // Make a request to the website
+    const { data: html } = await axios.get('https://www.example.com');
+  
+    // Parse the HTML
+    const $ = cheerio.load(html);
+  
+    // Select the products
+    const products = $('.sp-product-card').map((i, el) => {
+      return {
+        title: $(el).find('.product-details__title').text().trim(),
+        price: $(el).find('.product-info__price').text().trim(),
+        imageUrl: $(el).find('img').attr('data-src'),
+      };
+    }).get();
+  
+    // Send the products as the response
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.toString() });
+  }
+};
+
  
