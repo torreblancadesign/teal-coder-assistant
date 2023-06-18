837e9422e11bbf99cb576de9167d1b7ce1995f51
Enrique Torreblanca <enriquetchat@gmail.com>
2023-06-18 00:22:13 -0700

    File updated, no comment added

--- pages/api/api.js
+++ pages/api/api.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
     });
 
     // Make a request to the website
-    const { data: html } = await axiosInstance.get('https://creativemarket.com/graphics/objects/abstract?sort=popular&page=1');
+    const { data: html } = await axiosInstance.get('https://airvues.com');
  
     // Parse the HTML
     const $ = cheerio.load(html);
